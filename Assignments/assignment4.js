6eb4a64ec106ab039759cdf39f384d8cc9dd6c85
mdmahbubekhuda <md.mahbub.e@gmail.com>
2023-08-09 19:29:44 +0600

    assignment done

--- Assignments/assignment4.js
+++ Assignments/assignment4.js
@@ -31,11 +31,11 @@ function sortMaker(arr) {
     if (arr.length > 2) {
         return 'Please provide only 2 numbers in the array'
     }
-    else if (arr[0] === arr[1]) {
-        return 'equal';
-    }
     else if (arr[0] < 0 || arr[1] < 0) {
         return 'Invalid Input';
+    }
+    else if (arr[0] === arr[1]) {
+        return 'equal';
     }
     else if (arr[0] >= 0 && arr[1] >= 0) {
         function compareSort(a, b) {
