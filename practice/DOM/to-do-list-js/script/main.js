b8f05c30e116439dcf63acf61d7ea980a0701de7
mdmahbubekhuda <md.mahbub.e@gmail.com>
2023-08-21 20:26:15 +0600

    to-do fixed

--- practice/DOM/to-do-list-js/script/main.js
+++ practice/DOM/to-do-list-js/script/main.js
@@ -5,31 +5,40 @@ function getInputValue(inputId) {
     selectInput.value = ''
     return inputText
 }
+// function set task
+function setTask() {
+    // validation for empty input value
+    const getTask = getInputValue('#task')
+    if (getTask === '') return alert('No task is assigned')
 
-
-// add task
-document.querySelector('#btn-add').addEventListener('click', function () {
+    // add element
     const selectTable = document.querySelector('#table-body')
-
-    // add index
+    // add index number
     const count = selectTable.childElementCount
-
     // create element
     const tableRow = document.createElement('tr')
-
-    // set task
-    const getTask = getInputValue('#task')
-
+    tableRow.classList.add('dynamic-row')
     tableRow.innerHTML = `
     <td id="td-number"> ${count + 1} </td>
     <td id="td-description"> ${getTask} </td>
-    <td>
+    <td class="flex gap-4 flex-row">
         <button id="btn-done" class="btn btn-success">Done</button>
         <button id="btn-delete" class="btn btn-error">Delete</button>
     </td>`
-
     // append element
     selectTable.appendChild(tableRow)
+}
+
+// event handler
+document.querySelector('#to-do-list').addEventListener('click', function (e) {
+    // add task
+    if (e.target.matches('#btn-add')) setTask()
+    // done task
+    if (e.target.matches('#btn-done')) e.target.closest('.dynamic-row').style.textDecoration = 'line-through'
+    // delete task
+    if (e.target.matches('#btn-delete')) e.target.closest('.dynamic-row').remove()
+    // clear all task
+    if (e.target.matches('#btn-clear')) document.querySelector('#table-body').innerHTML = ''
 
 })
 
