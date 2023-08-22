ef3052b2778729499bee017fba38375139dd0f07
mdmahbubekhuda <md.mahbub.e@gmail.com>
2023-08-22 11:06:43 +0600

    to-do-list done

--- practice/DOM/to-do-list-js/script/main.js
+++ practice/DOM/to-do-list-js/script/main.js
@@ -5,7 +5,6 @@ function getInputValue(inputId) {
     selectInput.value = ''
     return inputText
 }
-
 // function set task
 function setTask() {
     // validation for empty input value
@@ -19,7 +18,7 @@ function setTask() {
     const tableRow = document.createElement('tr')
     tableRow.classList.add('dynamic-row')
     tableRow.innerHTML = `
-    <td id="td-number"> ${count + 1} </td>
+    <td class="td-number"> ${count + 1} </td>
     <td id="td-description"> ${getTask} </td>
     <td class="flex gap-4 flex-row">
         <button id="btn-done" class="btn btn-success">Done</button>
@@ -31,14 +30,14 @@ function setTask() {
 
 }
 
-// event handler
+// event handler to-do-list
 document.querySelector('#to-do-list').addEventListener('click', function (e) {
-    // add task
+    // add task by click (add button)
     if (e.target.matches('#btn-add')) setTask()
-    // key pressed - enter
+    // add task by key press (Enter and NumpadEnter)
     document.querySelector('#task').addEventListener('keyup', function (e) {
         e.stopImmediatePropagation()
-        if (e.code === 'Enter') setTask()
+        if (e.code === 'Enter' || e.code === 'NumpadEnter') setTask()
     })
     // done task
     if (e.target.matches('#btn-done')) e.target.closest('.dynamic-row').style.textDecoration = 'line-through'
@@ -46,9 +45,9 @@ document.querySelector('#to-do-list').addEventListener('click', function (e) {
     if (e.target.matches('#btn-delete')) {
         e.target.closest('.dynamic-row').remove()
         // fixing serial number
-        
+        let count = 1
+        document.querySelectorAll('.td-number').forEach((element) => element.innerText = count++)
     }
     // clear all task
     if (e.target.matches('#btn-clear')) document.querySelector('#table-body').innerHTML = ''
-
 })
