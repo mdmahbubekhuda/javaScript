403c6d4b2a067e5c9ac45a6e9e0a2c4896f26665
mdmahbubekhuda <md.mahbub.e@gmail.com>
2023-08-22 08:09:36 +0600

    serial number not fixed whend row is deleted

--- practice/DOM/to-do-list-js/script/main.js
+++ practice/DOM/to-do-list-js/script/main.js
@@ -5,6 +5,7 @@ function getInputValue(inputId) {
     selectInput.value = ''
     return inputText
 }
+
 // function set task
 function setTask() {
     // validation for empty input value
@@ -13,9 +14,8 @@ function setTask() {
 
     // add element
     const selectTable = document.querySelector('#table-body')
-    // add index number
-    const count = selectTable.childElementCount
     // create element
+    const count = selectTable.childNodes.length
     const tableRow = document.createElement('tr')
     tableRow.classList.add('dynamic-row')
     tableRow.innerHTML = `
@@ -25,20 +25,30 @@ function setTask() {
         <button id="btn-done" class="btn btn-success">Done</button>
         <button id="btn-delete" class="btn btn-error">Delete</button>
     </td>`
+
     // append element
     selectTable.appendChild(tableRow)
+
 }
 
 // event handler
 document.querySelector('#to-do-list').addEventListener('click', function (e) {
     // add task
     if (e.target.matches('#btn-add')) setTask()
+    // key pressed - enter
+    document.querySelector('#task').addEventListener('keyup', function (e) {
+        e.stopImmediatePropagation()
+        if (e.code === 'Enter') setTask()
+    })
     // done task
     if (e.target.matches('#btn-done')) e.target.closest('.dynamic-row').style.textDecoration = 'line-through'
     // delete task
-    if (e.target.matches('#btn-delete')) e.target.closest('.dynamic-row').remove()
+    if (e.target.matches('#btn-delete')) {
+        e.target.closest('.dynamic-row').remove()
+        // fixing serial number
+        
+    }
     // clear all task
     if (e.target.matches('#btn-clear')) document.querySelector('#table-body').innerHTML = ''
 
 })
-
